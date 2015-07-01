18a705d22956a9cf077cff5d9e8b99b0f271602b
Peter Ahinko <peter@ahinko.se>
2015-07-01 19:00:28 +0200

    Added ServerRequest Factory that creates Request objects

---
+++ src/Phapi/Http/ServerRequestFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Phapi\Http;
+
+use Zend\Diactoros\ServerRequestFactory as DiactorosServerRequestFactory;
+
+/**
+ * Class ServerRequestFactory
+ *
+ * @category Phapi
+ * @package  Phapi\Http
+ * @author   Peter Ahinko <peter@ahinko.se>
+ * @license  MIT (http://opensource.org/licenses/MIT)
+ * @link     https://github.com/phapi/
+ */
+class ServerRequestFactory extends DiactorosServerRequestFactory
+{
+
+    public static function fromGlobals(
+        array $server = null,
+        array $query = null,
+        array $body = null,
+        array $cookies = null,
+        array $files = null
+    ) {
+        $server  = static::normalizeServer($server ?: $_SERVER);
+        $files   = static::normalizeFiles($files ?: $_FILES);
+        $headers = static::marshalHeaders($server);
+        $request = new Request(
+            $server,
+            $files,
+            static::marshalUriFromServer($server, $headers),
+            static::get('REQUEST_METHOD', $server, 'GET'),
+            'php://input',
+            $headers
+        );
+        return $request
+            ->withCookieParams($cookies ?: $_COOKIE)
+            ->withQueryParams($query ?: $_GET)
+            ->withParsedBody($body ?: $_POST);
+    }
+
+}
